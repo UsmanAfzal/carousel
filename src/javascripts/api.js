eda7c2839741cfd9641175e7c7b1f1ad54769010
Usman Afzal <afzalu@parliament.uk>
2019-03-04 22:40:05 +0000

    Decoupled api from index.js

---
+++ src/javascripts/api.js
@@ -0,0 +1,4 @@
+const apiKey = '9656065-a4094594c34f9ac14c7fc4c39';
+const landscapeImages = 'https://pixabay.com/api/?key=' + apiKey + '&q=beautiful+landscape&image_type=photo';
+
+export { apiKey, landscapeImages }
