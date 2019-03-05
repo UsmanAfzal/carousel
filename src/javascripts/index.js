4c1159aaf7b87a005433d1e043b487362b2ebfbd
Usman Afzal <afzalu@parliament.uk>
2019-03-05 01:58:32 +0000

    Added alt text to images from api

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -8,13 +8,14 @@ window.addEventListener('load', () => {
 async function fetchImages() {
   const response = await fetch(landscapeImages);
   const json = await response.json();
+  console.log(json)
 
   /* Build Carousel */
   document.querySelector('main').innerHTML = `
     <div class="carousel">
       <ul>
         ${json.hits.map(function(item) {
-          return `<li><img src="${item.previewURL}"></li>`
+          return `<li><img src="${item.previewURL}" alt="${item.tags}"></li>`
         }).join('')}
       </ul>
       <div class="button--group">
